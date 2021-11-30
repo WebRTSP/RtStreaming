2518f3ba3ea1028cb627c37dae77718a82fa6d69
sergey radionov <rsatom@gmail.com>
2021-11-30 22:47:06 +0700

    It's still required to have fix for mDNS Ice Candidates
    since many users want use LTS Ubuntu version
    and GStreamer on the latest LTS Ubuntu (20.04)
    doesn't have mDNS Ice Candidates support

--- GstRtStreaming/GstWebRTCPeer2.cpp
+++ GstRtStreaming/GstWebRTCPeer2.cpp
@@ -56,6 +56,95 @@ GstWebRTCPeer2::GstWebRTCPeer2(
 
 namespace {
 
+void ResolveIceCandidate(
+    const std::string& candidate,
+    std::string* resolvedCandidate)
+{
+    if(!resolvedCandidate)
+        return;
+
+    resolvedCandidate->clear();
+
+    if(candidate.empty())
+        return;
+
+    enum {
+        ConnectionAddressPos = 4,
+        PortPos = 5,
+    };
+
+    std::string::size_type pos = 0;
+    unsigned token = 0;
+
+    std::string::size_type connectionAddressPos = std::string::npos;
+    std::string::size_type portPos = std::string::npos;
+    while(std::string::npos == portPos &&
+        (pos = candidate.find(' ', pos)) != std::string::npos)
+    {
+        ++pos;
+        ++token;
+
+        switch(token) {
+        case ConnectionAddressPos:
+            connectionAddressPos = pos;
+            break;
+        case PortPos:
+            portPos = pos;
+            break;
+        }
+    }
+
+    if(connectionAddressPos != std::string::npos &&
+        portPos != std::string::npos)
+    {
+        std::string connectionAddress(
+            candidate, connectionAddressPos,
+            portPos - connectionAddressPos - 1);
+
+        const std::string::value_type localSuffix[] = ".local";
+        if(connectionAddress.size() > (sizeof(localSuffix) - 1) &&
+           connectionAddress.compare(
+               connectionAddress.size() - (sizeof(localSuffix) - 1),
+               std::string::npos,
+               localSuffix) == 0)
+        {
+            if(hostent* host = gethostbyname(connectionAddress.c_str())) {
+                int ipLen;
+                switch(host->h_addrtype) {
+                case AF_INET:
+                    ipLen = INET_ADDRSTRLEN;
+                    break;
+                case AF_INET6:
+                    ipLen = INET6_ADDRSTRLEN;
+                    break;
+                default:
+                    return;
+                }
+
+                char ip[ipLen];
+                if(nullptr == inet_ntop(
+                    host->h_addrtype,
+                    host->h_addr_list[0],
+                    ip, ipLen))
+                {
+                   return;
+                }
+
+                if(resolvedCandidate) {
+                    ipLen = strlen(ip);
+
+                    *resolvedCandidate = candidate.substr(0, connectionAddressPos);
+                    resolvedCandidate->append(ip, ipLen);
+                    resolvedCandidate->append(
+                        candidate,
+                        portPos - 1,
+                        std::string::npos);
+                }
+            }
+        }
+    }
+}
+
 struct TeardownData
 {
     std::atomic_flag guard;
@@ -772,10 +861,14 @@ void GstWebRTCPeer2::addIceCandidate(
         return;
     }
 
-    if(!candidate.empty()) {
+    std::string resolvedCandidate;
+    if(!candidate.empty())
+        ResolveIceCandidate(candidate, &resolvedCandidate);
+
+    if(!resolvedCandidate.empty()) {
         g_signal_emit_by_name(
             rtcbin, "add-ice-candidate",
-            mlineIndex, candidate.c_str());
+            mlineIndex, resolvedCandidate.c_str());
     } else {
         g_signal_emit_by_name(
             rtcbin, "add-ice-candidate",
