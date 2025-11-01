bba0b14a1d13b7f7a6b9126451f7f65a639735bd
sergey radionov <rsatom@gmail.com>
2025-11-01 12:30:09 +0700

    GstClient: don't sync video frames to clock
    to prevents frames drop

--- GstRtStreaming/GstClient.cpp
+++ GstRtStreaming/GstClient.cpp
@@ -62,7 +62,7 @@ void GstClient::prepare(const WebRTCConfigPtr& webRTCConfig) noexcept
                 gst_parse_bin_from_description(
                     "rtph264depay ! avdec_h264 ! "
                     "videoconvert ! queue ! "
-                    "autovideosink", TRUE, NULL);
+                    "autovideosink sync=false", TRUE, NULL);
             gst_bin_add(GST_BIN(pipeline), out);
             gst_element_sync_state_with_parent(out);
 
@@ -74,7 +74,7 @@ void GstClient::prepare(const WebRTCConfigPtr& webRTCConfig) noexcept
                 gst_parse_bin_from_description(
                     "rtpvp8depay ! vp8dec ! "
                     "videoconvert ! queue ! "
-                    "autovideosink", TRUE, NULL);
+                    "autovideosink sync=false", TRUE, NULL);
             gst_bin_add(GST_BIN(pipeline), out);
             gst_element_sync_state_with_parent(out);
 
