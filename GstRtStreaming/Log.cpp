20fff6d9b888f22762e0160529d599306bfedf6c
sergey radionov <rsatom@gmail.com>
2025-11-10 13:54:56 +0700

    Log: refactored

--- GstRtStreaming/Log.cpp
+++ GstRtStreaming/Log.cpp
@@ -6,23 +6,28 @@
 
 static std::shared_ptr<spdlog::logger> Logger;
 
+
 void InitGstRtStreamingLogger(spdlog::level::level_enum level)
 {
-    spdlog::sink_ptr sink = std::make_shared<spdlog::sinks::stdout_sink_st>();
-
-    Logger = std::make_shared<spdlog::logger>("GstRtStreaming", sink);
+    if(!Logger) {
+        Logger = spdlog::stdout_logger_st("GstRtStreaming");
+#ifdef SNAPCRAFT_BUILD
+        Logger->set_pattern("[%n] [%l] %v");
+#endif
+    }
 
     Logger->set_level(level);
 }
 
 const std::shared_ptr<spdlog::logger>& GstRtStreamingLog()
 {
-    if(!Logger)
-#ifndef NDEBUG
-        InitGstRtStreamingLogger(spdlog::level::debug);
-#else
+    if(!Logger) {
+#ifdef NDEBUG
         InitGstRtStreamingLogger(spdlog::level::info);
+#else
+        InitGstRtStreamingLogger(spdlog::level::debug);
 #endif
+    }
 
     return Logger;
 }
